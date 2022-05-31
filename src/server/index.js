ef66ecaa949db833953d6953d9fbc54e53c1939e
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-05-31 13:28:21 +0200

    Added not found and general error middlewares

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const userRouter = require("./routers/userRouter");
+const { notFoundError, generalError } = require("./middlewares/errors/errors");
 
 const app = express();
 
@@ -12,5 +13,8 @@ app.use(helmet());
 app.use(express.json());
 
 app.use("/user", userRouter);
+
+app.use(notFoundError);
+app.use(generalError);
 
 module.exports = app;
---
+++ src/server/middlewares/errors/errors.js
@@ -0,0 +1,19 @@
+const chalk = require("chalk");
+const debug = require("debug")("padelbookings:server:middlewares:errors");
+
+const notFoundError = (req, res) => {
+  res.status(404).json({ msg: "Endpoint not found" });
+};
+
+// eslint-disable-next-line no-unused-vars
+const generalError = (error, req, res, next) => {
+  const errorStatusCode = error.statusCode ?? 500;
+  const errorMessage = error.customMessage ?? "General error";
+  debug(chalk.red(`Error: ${errorMessage}`));
+  res.status(errorStatusCode).json({ msg: errorMessage });
+};
+
+module.exports = {
+  notFoundError,
+  generalError,
+};
