a476a49f5f7ac04cfb0eb42b2b5e7f8cfae86205
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-05-31 12:18:13 +0200

    Added cors,morgan,helmet and register(with enpoint route)

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const userRouter = require("./routers/userRouter");
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.json());
+
+app.use("/user", userRouter);
 
 module.exports = app;
