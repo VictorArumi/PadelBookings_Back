441645841c6166b1e8f8f0fe44fe7b8240fc6e62
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-01 17:58:02 +0200

    Tested login endpoint

--- src/server/routers/userRouter.test.js
+++ src/server/routers/userRouter.test.js
@@ -21,16 +21,91 @@ afterAll(async () => {
   await mongoServer.stop();
 });
 
+const newUserData = {
+  username: "testusername",
+  password: "newpassword",
+  name: "testname",
+  profilePicture: "testpicture.jpg",
+};
+
 describe("Given a POST /users/register/ endpoint", () => {
   describe("When it receives a request with a valid new user", () => {
     test("Then it should respond with 201 status code", async () => {
-      const newUserData = {
+      await request(app).post("/user/register").send(newUserData).expect(201);
+    });
+  });
+
+  describe("When it receives a request with a wrong new user", () => {
+    test("Then it should respond with 400 status code and the created user", async () => {
+      const badNewUserData = {
         username: "testusername",
-        password: "newpassword",
+        wrongproperty: "newpassword",
         name: "newname",
       };
 
+      await request(app)
+        .post("/user/register")
+        .send(badNewUserData)
+        .expect(400);
+    });
+  });
+
+  describe("When it receives a request with a user that already exists", () => {
+    test("Then it should respond with 409 status code", async () => {
+      await User.create(newUserData);
+
+      await request(app).post("/user/register").send(newUserData).expect(409);
+    });
+  });
+});
+
+describe("Given a POST /users/login/ endpoint", () => {
+  describe("When it receives a request with an existing user and it's right password", () => {
+    test("Then it should respond with a 200 status code", async () => {
       await request(app).post("/user/register").send(newUserData).expect(201);
+
+      await request(app).post("/user/login").send(newUserData).expect(200);
+    });
+  });
+
+  describe("When it receives a request with a wrong username", () => {
+    test("Then it should respond with a 403 status code", async () => {
+      await request(app).post("/user/register").send(newUserData).expect(201);
+
+      await request(app)
+        .post("/user/login")
+        .send({
+          username: "wrongUsername",
+          password: newUserData.password,
+          name: newUserData.name,
+        })
+        .expect(403);
+    });
+  });
+
+  describe("When it receives a request with a wrong password", () => {
+    test("Then it should respond with a 403 status code", async () => {
+      await request(app).post("/user/register").send(newUserData).expect(201);
+
+      await request(app)
+        .post("/user/login")
+        .send({
+          username: newUserData.username,
+          password: "wrong password",
+          name: newUserData.name,
+        })
+        .expect(403);
+    });
+  });
+
+  describe("When it receives a request with wrong properties", () => {
+    test("Then it should respond with a 400 status code", async () => {
+      await request(app).post("/user/register").send(newUserData).expect(201);
+
+      await request(app)
+        .post("/user/login")
+        .send({ wrong: "wrong" })
+        .expect(400);
     });
   });
 });
