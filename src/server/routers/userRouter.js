cc69c65f8b7e43dbaec4254cbe07dbf3630019d9
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-02 19:37:46 +0200

    Added joi validation in register

--- src/server/routers/userRouter.js
+++ src/server/routers/userRouter.js
@@ -1,9 +1,17 @@
+require("dotenv").config();
 const express = require("express");
+const { validate } = require("express-validation");
 const { userRegister, userLogin } = require("../controllers/userControllers");
+const userRegisterCredentialsSchema = require("../schemas/userCredentialsSchema");
 
 const userRouter = express.Router();
 
-userRouter.post("/register", userRegister);
+userRouter.post(
+  "/register",
+  validate(userRegisterCredentialsSchema),
+  userRegister
+);
+
 userRouter.post("/login", userLogin);
 
 module.exports = userRouter;
---
+++ src/server/schemas/userCredentialsSchema.js
@@ -0,0 +1,19 @@
+const { Joi } = require("express-validation");
+
+const userRegisterCredentialsSchema = {
+  body: Joi.object({
+    username: Joi.string()
+      .max(20)
+      .messages({ message: "Username is required" })
+      .required(),
+    password: Joi.string()
+      .min(4)
+      .max(20)
+      .messages({ message: "Password is required" })
+      .required(),
+    name: Joi.string().min(4).max(20),
+    profilePicture: Joi.string().allow(null, ""),
+  }),
+};
+
+module.exports = userRegisterCredentialsSchema;
