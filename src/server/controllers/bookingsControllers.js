9cb0c49bc85cf0f21bdbb29cc3b8a0e2866be0d0
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-08 15:47:36 +0200

    Added createBooking controller, added debugs to other controllers

--- src/server/controllers/bookingsControllers.js
+++ src/server/controllers/bookingsControllers.js
@@ -1,13 +1,30 @@
-const Booking = require("../../database/models/Booking");
-
 require("dotenv").config();
+const debug = require("debug")("padelbookings:server:booking-controllers");
+const chalk = require("chalk");
+const Booking = require("../../database/models/Booking");
 
 const getBookings = async (req, res, next) => {
   try {
     const bookings = await Booking.find();
     res.status(200).json({ bookings });
+    debug(chalk.green(`Bookings list delivered`));
   } catch {
     next();
+  }
+};
+
+const createBooking = async (req, res, next) => {
+  try {
+    const newBookingData = req.body;
+
+    const createdNewBooking = await Booking.create(newBookingData);
+
+    res.status(201).json({ createdBooking: createdNewBooking });
+    debug(chalk.green(`New booking ${createdNewBooking.id} created`));
+  } catch (error) {
+    error.statusCode = 400;
+    error.customMessage = "Bad request";
+    next(error);
   }
 };
 
@@ -25,6 +42,7 @@ const deleteBooking = async (req, res, next) => {
       return;
     }
     res.status(200).json({ msg: `Item with id ${id} has been deleted` });
+    debug(chalk.green(`Booking ${id} has been deleted`));
   } catch (error) {
     error.statusCode = 400;
     error.customMessage = "Bad request";
@@ -32,4 +50,4 @@ const deleteBooking = async (req, res, next) => {
   }
 };
 
-module.exports = { getBookings, deleteBooking };
+module.exports = { getBookings, deleteBooking, createBooking };
