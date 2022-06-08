ad0e8240c92712bfefa10829cfeb651a0f395e42
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-08 17:21:17 +0200

    Ammended regex caps error

--- src/server/schemas/newBookingSchema.js
+++ src/server/schemas/newBookingSchema.js
@@ -6,34 +6,26 @@ const newBookingSchema = {
       .max(20)
       .messages({ message: "Club is required" })
       .required(),
-
     owner: Joi.string().messages({ message: "Owner is required" }).required(),
-
     date: Joi.string()
       .min(10)
       .max(10)
       .messages({ message: "Date is required" })
       .required(),
-
     hour: Joi.string()
       .min(2)
       .max(20)
       .messages({ message: "Hour is required" })
       .required(),
-
     courtType: Joi.string()
-      .regex(/^(indoor|outdoor)$/)
       .messages({ message: "CourtType is required" })
+      .regex(/^(Outdoor|Indoor)$/)
       .required(),
-
     players: Joi.array()
       .max(4)
       .messages({ message: "Players is required" })
       .required(),
-
-    open: Joi.boolean()
-      .messages({ message: "Reserve open/closed status is required" })
-      .required(),
+    open: Joi.boolean().required(),
   }),
 };
 
