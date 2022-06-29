32851a291e08b762d5f58b61d602dda564d4de45
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-29 18:11:56 +0200

    Added new middleware in detail endpoint

--- src/server/routers/bookingsRouter.js
+++ src/server/routers/bookingsRouter.js
@@ -8,6 +8,7 @@ const {
   editBooking,
   getBooking,
   editBookingPlayers,
+  getBookingAndPlayersUsernames,
 } = require("../controllers/bookingsControllers");
 const auth = require("../middlewares/auth/auth");
 const newBookingSchema = require("../schemas/newBookingSchema");
@@ -15,7 +16,12 @@ const newBookingSchema = require("../schemas/newBookingSchema");
 const bookingsRouter = express.Router();
 
 bookingsRouter.get("/limit=:limit&page=:page", auth, getBookings);
-bookingsRouter.get("/detail/:id", auth, getBooking);
+bookingsRouter.get(
+  "/detail/:id",
+  auth,
+  getBooking,
+  getBookingAndPlayersUsernames
+);
 bookingsRouter.delete("/:id", auth, deleteBooking);
 bookingsRouter.post("/create", auth, validate(newBookingSchema), createBooking);
 bookingsRouter.put("/edit/:id", auth, validate(newBookingSchema), editBooking);
