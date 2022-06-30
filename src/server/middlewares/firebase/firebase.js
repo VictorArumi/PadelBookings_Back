113bf710bd09ebd7d357b9842e0f612c5db0532f
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-06-30 11:26:35 +0200

    Refactorized firebase error management

--- src/server/middlewares/firebase/firebase.js
+++ src/server/middlewares/firebase/firebase.js
@@ -1,70 +1,71 @@
-const debug = require("debug")("padelbookings:server:firebase");
-const chalk = require("chalk");
-const { initializeApp } = require("firebase/app");
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
 const {
   getStorage,
+  ref,
   uploadBytes,
   getDownloadURL,
-  ref,
 } = require("firebase/storage");
+const { initializeApp } = require("firebase/app");
 
 const firebase = async (req, res, next) => {
   const firebaseConfig = {
-    apiKey: "AIzaSyAtkxRzVeyXKyRrTVGjpaQbO1pNx9DU8b8",
-    authDomain: "padelbookings-75152.firebaseapp.com",
-    projectId: "padelbookings-75152",
-    storageBucket: "padelbookings-75152.appspot.com",
-    messagingSenderId: "226596812228",
-    appId: "1:226596812228:web:1ab07f4dcad13a52682091",
+    apiKey: "AIzaSyAozs25IxSHFCKJVb-KZZNazikPO3hg8z0",
+    authDomain: "tootattoo-images.firebaseapp.com",
+    projectId: "tootattoo-images",
+    storageBucket: "tootattoo-images.appspot.com",
+    messagingSenderId: "617428563723",
+    appId: "1:617428563723:web:35f6da80e29b4a4879540e",
   };
 
-  const firebaseApp = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
 
   const { file } = req;
 
-  const newFileName = file ? `${Date.now()}${file.originalname}` : "";
+  const newPicture = file ? `${Date.now()}${file.originalname}` : "";
+
   try {
-    if (file) {
-      fs.rename(
+    if (!file) {
+      req.profilePicture = "";
+      req.profilePictureBackup = "";
+      next();
+    } else {
+      await fs.rename(
         path.join("uploads", "images", file.filename),
-        path.join("uploads", "images", newFileName),
+        path.join("uploads", "images", `${Date.now()}${file.originalname}`),
         async (error) => {
           if (error) {
-            debug(chalk.red("Error renaming profile picture file"));
-
             next(error);
-            return;
           }
-          req.body.profilePicture = newFileName;
 
-          fs.readFile(
-            path.join("uploads", "images", newFileName),
+          await fs.readFile(
+            path.join("uploads", "images", newPicture),
             async (readError, readFile) => {
               if (readError) {
-                debug(chalk.red("Error reading post picture"));
-
                 next(readError);
-                return;
               }
-              const storage = getStorage(firebaseApp);
 
-              const storageRef = ref(storage, newFileName);
+              const storage = getStorage(app);
+              const storageRef = ref(storage, newPicture);
+
               await uploadBytes(storageRef, readFile);
-              const firebaseFileURL = await getDownloadURL(storageRef);
 
-              req.body.profilePictureBackup = firebaseFileURL;
+              const firebaseImageURL = await getDownloadURL(storageRef);
+
+              req.profilePictureBackup = firebaseImageURL;
+              req.profilePicture = path.join("uploads", "images", newPicture);
 
               next();
             }
           );
         }
       );
-    } else {
-      next();
     }
-  } catch (error) {
+  } catch {
+    const error = new Error("Image backup unsuccesful");
+    error.statusCode = 400;
+    error.customMessage = "Image backup unsuccesful";
+
     next(error);
   }
 };
